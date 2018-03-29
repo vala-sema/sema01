29ce90d86bbeb1314cd614ec9158d4c80ffa61ee
Vassily <vasil.deville@gmail.com>
2018-03-29 13:43:51 +0300

    add a ut

--- app01/tests.py
+++ app01/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class SomeBasicTest(TestCase):
+    def test_text_correct(self):
+        response = self.client.get('/')
+        self.assertIn('Welcome', response)

d78fa8494c95acf18a1fdc5f12234c912a0ca6c9
Vassily <vasil.deville@gmail.com>
2018-03-29 13:53:02 +0300

    delete ut

--- app01/tests.py
+++ app01/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
-class SomeBasicTest(TestCase):
-    def test_text_correct(self):
-        response = self.client.get('/')
-        self.assertIn('Welcome', response)
+# class SomeBasicTest(TestCase):
+#     def test_text_correct(self):
+#         response = self.client.get('/')
+#         self.assertIn('Welcome', response)
